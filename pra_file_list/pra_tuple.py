50c4972f6c238cb484906c736e4a1e1e9577bfc5
selpeapi <selpeapi@gmail.com>
2022-02-25 01:05:22 +0900

    튜플 추가

---
+++ pra_file_list/pra_tuple.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+
+    # 튜플은 리스트와 달리 내부의 값을 건들 수 없음
+    # 튜플을 만들 땐 "( )"를 사용
+    tuple_alphabet =('aaa', 'bbb', 'ccc')
+    print(tuple_alphabet)
+    # -> ('aaa', 'bbb', 'ccc')
+
+    print(tuple_alphabet[0 : 1])
+    # -> ('aaa',)
