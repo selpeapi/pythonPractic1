755eb35a3eda77d08381e32dc6ec7a6304efee1b
selpeapi <selpeapi@gmail.com>
2022-02-26 02:06:45 +0900

    for문 추가

---
+++ pra_file_list/if_while_for/pra_for.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+
+    # for문
+    a =0
+    for b in range(1,11):
+        print(a+b, end='')
+    print()
+
+    str_list =['aaa', 'bbb', 'ccc', 'ddd', 'eee']
+    for b in str_list:
+        print(b)
+
+    c =[[1,2,3],4]
+    print(c)
+
+    d =range(10)
+    print(d)
