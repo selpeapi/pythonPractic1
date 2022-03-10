6a7d3ebeaeab0ee11f5140a4d4ef00b62f2d5aef
selpeapi <selpeapi@gmail.com>
2022-03-10 14:00:09 +0900

    module 추가

---
+++ pra_file_list/class_module_package/pra_module.py
@@ -0,0 +1,16 @@
+import pra_class
+
+a =111
+# 모듈을 import 해서 불러올 때, '__name__' 아래에 구현된 코드는 false 값을 받아 불러오지 않음
+# '__name__' 에 구현된 해당 코드는 본인 파일에서만 ture 값으로 실행 됨
+# 해당 파일에서는 'main' 값이, 다른 파일에서는 '이름.py' 에 해당하는 이름이 불러와짐
+if __name__ == '__main__':
+    print(a)
+    # -> 111
+    b =pra_class.calculator(1,2)
+    print(b.plus())
+    # -> 3
+    print(pra_class.__name__)
+    # -> pra_class
+    print(__name__)
+    # -> __main__
