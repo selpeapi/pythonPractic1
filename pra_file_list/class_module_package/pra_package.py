cadd37ebfdabef732eb261a280e6a18b8f981dcf
selpeapi <selpeapi@gmail.com>
2022-03-10 19:03:09 +0900

    package 추가

---
+++ pra_file_list/class_module_package/pra_package.py
@@ -0,0 +1,16 @@
+import pra_file_list.class_module_package.pra_module
+from pra_file_list.class_module_package.pra_class import calculator2
+from pra_file_list.class_module_package import pra_class
+
+a =pra_file_list.class_module_package.pra_module
+print(a.a)
+# -> 111
+pra_file_list.class_module_package.pra_module.a =222
+print(pra_file_list.class_module_package.pra_module.a)
+# -> 222
+
+b =calculator2(1,2)
+print(b.plus())
+
+c =pra_class.lowCalculator(3,0)
+print(c.divide())
