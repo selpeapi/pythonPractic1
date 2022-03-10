f9b60bbaa98fc48a6cfe463d4ac00d93f898036a
selpeapi <selpeapi@gmail.com>
2022-03-10 13:59:36 +0900

    class 편집

--- pra_file_list/class_module_package/pra_class.py
+++ pra_file_list/class_module_package/pra_class.py
@@ -1,28 +1,68 @@
+import pra_module
+
+print(pra_module.a)
+
+
+class calculator:
+    # __init__은 생성자
+    def __init__(self, num0, num1):
+        self.num0 = num0
+        self.num1 = num1
+
+    def inputnum(self, num0, num1):
+        self.num0 = num0
+        self.num1 = num1
+
+    def plus(self):
+        result = self.num0 + self.num1
+        return result
+
+    def minus(self):
+        result = self.num0 - self.num1
+        return result
+
+    def multiply(self):
+        result = self.num0 * self.num1
+        return result
+
+    def divide(self):
+        result = self.num0 / self.num1
+        return result
+
+
+# 상속
+class lowCalculator(calculator):
+    # 메소드 오버라이딩
+    def divide(self):
+        if self.num1 == 0:
+            return 0
+        else:
+            result = self.num0 / self.num1
+            return result
+
+
+# 클래스 변수
+class classvar():
+    aaa = 0
+
+
+class calculator2:
+    # __init__은 생성자
+    def __init__(self, num0, num1):
+        self.num0 = num0
+        self.num1 = num1
+
+    def inputnum(self, num0, num1):
+        self.num0 = num0
+        self.num1 = num1
+
+    def plus(self):
+        result = self.num0 + self.num1
+        return result
+
+
 if __name__ == '__main__':
-    #
-    class calculator:
-        # __init__은 생성자
-        def __init__(self, num0, num1):
-            self.num0 =num0
-            self.num1 =num1
-        def inputnum(self, num0, num1):
-            self.num0 =num0
-            self.num1 =num1
-        def plus(self):
-            result =self.num0 +self.num1
-            return result
-        def minus(self):
-            result =self.num0 -self.num1
-            return result
-        def multiply(self):
-            result =self.num0 *self.num1
-            return result
-        def divide(self):
-            result =self.num0 /self.num1
-            return result
-
-
-    cal1 =calculator(2,3)
+    cal1 = calculator(2, 3)
 
     print(cal1.num0, cal1.num1)
     print(cal1.plus())
@@ -30,28 +70,14 @@ if __name__ == '__main__':
     print(cal1.multiply())
     # print(cal1.divide())
 
-    # 상속
-    class lowCalculator(calculator):
-        # 메소드 오버라이딩
-        def divide(self):
-            if self.num1 ==0:
-                return 0
-            else:
-                result =self.num0 /self.num1
-                return result
-
-    # 클래스 변수
-    class classvar():
-        aaa =0
-
-    a =classvar()
+    a = classvar()
     print(a.aaa)
     # -> 0
-    a.aaa =1
+    a.aaa = 1
     print(a.aaa)
     # -> 1
-    classvar.aaa =2
-    b =classvar()
+    classvar.aaa = 2
+    b = classvar()
     print(b.aaa)
     # -> 2
     print(a.aaa)
